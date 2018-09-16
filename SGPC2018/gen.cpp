607a355288d4d2432f2214695cd85171c515811d
marmot0814 <s1357andy871207@gmail.com>
2018-09-16 12:51:13 +0800

    H WA

--- SGPC2018/gen.cpp
+++ SGPC2018/gen.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int n; n = 30; cout << n << '\n';
-    for (int i = 0 ; i < n ; i++) {
-        for (int j = 0 ; j < n ; j++)
-            cout << rand() % 1000000 + 1 << ' ';
-        cout << '\n';
-    }
+    int n, k; n = 50, k = 125000 - 49 * 49 * 49;
+    cout << n << ' ' << k << '\n';
+    for (int i = 0 ; i < k ; i++)
+        cout << i + 1 << ' ';
+    cout << '\n';
 }
